fd3aa5bedaed85077e4312692ce93f97644db9a2
podmar <marta.podziewska@gmail.com>
2023-02-01 15:41:45 +0100

    fix a bug in condition on request body and change number type to string

--- pages/api/entry.ts
+++ pages/api/entry.ts
@@ -3,7 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   message?: string,
-  remaining_entries?: number
+  remaining_entries?: string
 }
 
 export default function handler(
@@ -13,18 +13,19 @@ export default function handler(
 
   if (req.method === 'GET') {
     res.status(200).json({
-      remaining_entries: 10
+      remaining_entries: "10"
     })
 
-  } else {
+  } else if (req.method === 'POST')  {
 
-    if (req.body.number || !req.body.date) {
+    if (!req.body.number || !req.body.date) {
+      console.log(req.body.number, req.body.date)
       return res.status(400).json({ message: 'Number of entries or date nor found' })
     }
 
     res.status(200).json({
       message: `${req.body.number} entry/entries recorded on ${req.body.date}`,
-      remaining_entries: 10 - req.body.number
+      remaining_entries: (10 - parseInt(req.body.number)).toString()
     })
   }
 }
