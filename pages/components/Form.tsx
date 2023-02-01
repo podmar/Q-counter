953dadf7c33fc990093bfc95f7cd8f9b52ab430d
podmar <marta.podziewska@gmail.com>
2023-02-01 14:30:54 +0100

    add props, write submit form function and fix endpoint

--- pages/components/Form.tsx
+++ pages/components/Form.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
 
-function Form() {
+function Form({setEntries}: FormProps) {
+    const today = new Date().toLocaleString('en-CA')
+
+    const submitForm = async () => {
+
+        const response = await fetch("http://localhost:3000/api/entry/", {
+            method: "post",
+        })
+        const data = await response.json()
+        setEntries(data.remaining_entries)
+        console.log(data.message)
+    }
+
   return (
       <>
-        <form action="/pages/api/entry.ts" method="post">
+        <form action="http://localhost:3000/api/entry/" method="post">
             <div>
               <label htmlFor="number">Number of enries to deduct</label>
               <input type="text" id="number" name="number" defaultValue={1}/>
             </div>
             <div>
               <label htmlFor="date">Date of entry:</label>
-              <input type="date" id="date" name="date" defaultValue={new Date().toLocaleDateString()}/>
+              <input type="date" id="date" name="date" defaultValue={today}/>
             </div>
 
           <button type="submit">Submit</button>
