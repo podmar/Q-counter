4b201d7483e2a169da6b17529ddd2080b4a2861d
podmar <marta.podziewska@gmail.com>
2023-02-01 15:43:11 +0100

    create requestOptions and add change events to the form

--- pages/components/Form.tsx
+++ pages/components/Form.tsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Form({setEntries}: FormProps) {
     const today = new Date().toLocaleString('en-CA')
 
-    const submitForm = async () => {
+    const [entryDate, setEntryDate] = useState(today)
+  const [entriesToDeduct, setEntriesToDeduct] = useState("1")
 
-        const response = await fetch("http://localhost:3000/api/entry/", {
-            method: "post",
-        })
+  const requestOptions: RequestInit = {
+    method: "POST",
+    body: JSON.stringify({
+      number: entriesToDeduct,
+      date: entryDate
+    })
+
+  }
+
+  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    console.log(requestOptions)
+        const response = await fetch("http://localhost:3000/api/entry/", requestOptions)
         const data = await response.json()
         setEntries(data.remaining_entries)
         console.log(data.message)
@@ -15,14 +26,14 @@ function Form({setEntries}: FormProps) {
 
   return (
       <>
-        <form action="http://localhost:3000/api/entry/" method="post">
+        <form onSubmit={(e) => submitForm(e)} method="post">
             <div>
               <label htmlFor="number">Number of enries to deduct</label>
-              <input type="text" id="number" name="number" defaultValue={1}/>
+              <input onChange={(e) => setEntriesToDeduct(e.target.value)} type="number" id="number" name="number" defaultValue={entriesToDeduct}/>
             </div>
             <div>
               <label htmlFor="date">Date of entry:</label>
-              <input type="date" id="date" name="date" defaultValue={today}/>
+              <input onChange={(e) => setEntryDate(e.target.value)} type="date" id="date" name="date" defaultValue={entryDate}/>
             </div>
 
           <button type="submit">Submit</button>
