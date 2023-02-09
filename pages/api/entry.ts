dc3e4ac364cd6941e816fce28125fc7eb53b5f3c
podmar <marta.podziewska@gmail.com>
2023-02-09 12:48:44 +0100

    comment out not functional database connection

--- pages/api/entry.ts
+++ pages/api/entry.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import connectDB from '@/middleware/mongoDBConfig';
-import Entry from '@/models/entryModel';
+// import Entry from '@/models/entryModel';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -24,24 +24,24 @@ export default function handler(
   return res.status(400).json({ message: 'Number of entries or date nor found' })
   }
 
-  try {
-    console.log('CONNECTING TO MONGO');
-    await connectDB();
-    console.log('CONNECTED TO MONGO');
+  // try {
+  //   console.log('CONNECTING TO MONGO');
+  //   await connectDB();
+  //   console.log('CONNECTED TO MONGO');
 
-    console.log('CREATING DOCUMENT');
-    const entry = await Entry.create(req.body);
-    const allEntries = await Entry.find({})
-    const totalEntries = Object.keys(allEntries).length
-    console.log('CREATED DOCUMENT');
+  //   console.log('CREATING DOCUMENT');
+  //   const entry = await Entry.create(req.body);
+  //   const allEntries = await Entry.find({})
+  //   const totalEntries = Object.keys(allEntries).length
+  //   console.log('CREATED DOCUMENT');
 
-    res.status(200).json({
-    message: `${req.body.number} entry/entries recorded on ${req.body.date}`,
-    remaining_entries: (10 - totalEntries).toString()
-    })
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ message: `Database error: ${error}` })
-  }
+  //   res.status(200).json({
+  //   message: `${req.body.number} entry/entries recorded on ${req.body.date}`,
+  //   remaining_entries: (10 - totalEntries).toString()
+  //   })
+  // } catch (error) {
+  //   console.log(error);
+  //   return res.status(400).json({ message: `Database error: ${error}` })
+  // }
 }
   }
