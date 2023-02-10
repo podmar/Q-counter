d700925da50884218aa59b7b5115ce72530a311e
podmar <marta.podziewska@gmail.com>
2023-02-10 17:33:15 +0100

    add console log statements

--- middleware/mongoDBConfig.ts
+++ middleware/mongoDBConfig.ts
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI
 
 const connectDB = async () => {
+    console.log('CONNECTING TO MONGO');
   try {
     await mongoose.connect(MONGODB_URI!);
+    console.log('CONNECTED TO MONGO');
+
   } catch (error) {
     console.log("Connecting to database failed.")
   }
