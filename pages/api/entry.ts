258faebb90e733daf789103ce2f55aa1128311ea
podmar <marta.podziewska@gmail.com>
2023-02-10 17:33:56 +0100

    restructure db query

--- pages/api/entry.ts
+++ pages/api/entry.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import connectDB from '@/middleware/mongoDBConfig';
+import Entry from '@/models/entryModel';
+import { create } from 'domain';
 // import Entry from '@/models/entryModel';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
@@ -12,6 +14,7 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // connectDB();
 
   if (req.method === 'GET') {
     res.status(200).json({
@@ -20,28 +23,27 @@ export default function handler(
   } else if (req.method === 'POST')  {
 
   if (!req.body.number || !req.body.date) {
-  console.log(req.body.number, req.body.date)
+  console.log(`The request body is ${req.body.number} ${req.body.date}`)
   return res.status(400).json({ message: 'Number of entries or date nor found' })
   }
 
-  // try {
-  //   console.log('CONNECTING TO MONGO');
-  //   await connectDB();
-  //   console.log('CONNECTED TO MONGO');
+  connectDB();
 
-  //   console.log('CREATING DOCUMENT');
-  //   const entry = await Entry.create(req.body);
-  //   const allEntries = await Entry.find({})
-  //   const totalEntries = Object.keys(allEntries).length
-  //   console.log('CREATED DOCUMENT');
+  const createEntry = async () => await Entry.create(req.body);
+  const getAllEntries = async () => await Entry.find({})
+  console.log('CREATING DOCUMENT');
 
-  //   res.status(200).json({
-  //   message: `${req.body.number} entry/entries recorded on ${req.body.date}`,
-  //   remaining_entries: (10 - totalEntries).toString()
-  //   })
-  // } catch (error) {
-  //   console.log(error);
-  //   return res.status(400).json({ message: `Database error: ${error}` })
-  // }
+  try {
+    createEntry()
+    const totalEntries = Object.keys(getAllEntries).length
+    console.log('CREATED DOCUMENT');
+    return res.status(200).json({
+      message: `${req.body.number} entry/entries recorded on ${req.body.date}`,
+      remaining_entries: (10 - totalEntries).toString()
+    })
+  } catch (error) {
+      console.log(error);
+      return res.status(400).json({ message: `Database error: ${error}` })
+    }
 }
   }
