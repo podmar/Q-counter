972a9b6ba9c6870d5cc9ad3e96d3f86a0377c920
podmar <marta.podziewska@gmail.com>
2023-02-15 16:55:43 +0100

    add reference to _id of a card

--- models/entryModel.ts
+++ models/entryModel.ts
@@ -1,9 +1,9 @@
-import { Schema, model, models } from 'mongoose';
+import mongoose, { Schema, model, models } from 'mongoose';
 
 const entrySchema = new Schema({
   date: String,
   number: Number,
-
+  card: { type: mongoose.Types.ObjectId , required: true, unique: true, ref: 'Card' },
 }, {timestamps: true });
 
 const Entry = models.Entry || model('Entry', entrySchema);
