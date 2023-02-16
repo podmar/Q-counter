d98cfcbe10aee6dc14e474bfed8d5195ea63f721
podmar <marta.podziewska@gmail.com>
2023-02-16 13:18:40 +0100

    add ifo and conditions about exisiting connection

--- lib/mongoDBConfig.ts
+++ lib/mongoDBConfig.ts
@@ -1,12 +1,26 @@
 import mongoose from "mongoose";
 
+interface Connection {
+  isConnected?: Number
+}
+
 const MONGODB_URI = process.env.MONGODB_URI
 
+const connection: Connection = {}
+
 const connectDB = async () => {
-    console.log('CONNECTING TO MONGO');
+  console.log('CONNECTING TO MONGO');
+  if (connection.isConnected) {
+    console.log(connection.isConnected)
+    return
+  }
+
   try {
-    await mongoose.connect(MONGODB_URI!);
+    const db = await mongoose.connect(MONGODB_URI!);
     console.log('CONNECTED TO MONGO');
+
+    connection.isConnected = db.connections[0].readyState;
+    console.log(connection.isConnected);
 
   } catch (error) {
     console.log("Connecting to database failed.")
