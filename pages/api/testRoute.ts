395561086ef7803ea9bdbd5c131b22aea6355770
podmar <marta.podziewska@gmail.com>
2023-02-16 16:17:20 +0100

    add req.body to responce

--- pages/api/testRoute.ts
+++ pages/api/testRoute.ts
@@ -3,10 +3,11 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   message: string,
+  req: any
 }
 
 connectDB();
 
 export default async (req:NextApiRequest, res:NextApiResponse<Data>) => {
-    res.json({message: "Test route functional."})
+    res.json({message: "Test route functional.", req: req.body})
 }
