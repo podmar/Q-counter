c0b79cca12b43adf8da54df1e2233b1d228aca18
podmar <marta.podziewska@gmail.com>
2023-02-16 16:44:28 +0100

    add body to search criteria and change the variable name to activeCards

--- pages/api/cards/index.ts
+++ pages/api/cards/index.ts
@@ -20,8 +20,8 @@ export default async function handler(
     switch (method) {
         case 'GET':
             try {
-                const cards = await Card.find({isActive: true});
-                res.status(200).json({ success: true, result: cards })
+                const activeCards = await Card.find({...body, isActive: true});
+                res.status(200).json({ success: true, result: activeCards })
             } catch (error) {
                 res.status(400).json({ success: false, error: error })
 
