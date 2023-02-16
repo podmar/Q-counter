96833e813c32d1fdbcbe967a0173e34772606e5a
podmar <marta.podziewska@gmail.com>
2023-02-16 16:24:23 +0100

    add userName

--- models/cardModel.ts
+++ models/cardModel.ts
@@ -1,11 +1,11 @@
 import { Schema, model, models, Types } from "mongoose";
 
 const cardSchema = new Schema({
+  userName: { type: String, required: [true, 'Please submit a valid user name.'] },
   date: { type: Date, default: Date.now },
   totalEntries: { type: Number, default: 10 },
   remainingEntries: { type: Number, default: 10 },
   usedEntries: [{ type: Types.ObjectId, ref: 'Entry' }],
-  userName: { type: String },
   isActive: { type: Boolean, default: true },
 });
 
