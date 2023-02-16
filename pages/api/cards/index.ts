bbf7a45c8962724aa8ad0d3fd51cbaae9ff62dd6
podmar <marta.podziewska@gmail.com>
2023-02-16 14:28:10 +0100

    draft the card route

---
+++ pages/api/cards/index.ts
@@ -0,0 +1,42 @@
+import connectDB from '@/lib/connectDB';
+import Card from '@/models/cardModel';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+// type CardData = {
+//   message?: string,
+// }
+
+connectDB();
+
+export default async function handler(
+    req: NextApiRequest,
+    // res: NextApiResponse<CardData>
+    res: NextApiResponse
+
+) {
+
+    const { method, body } = req;
+
+    switch (method) {
+        case 'GET':
+            try {
+                const cards = await Card.find({});
+                res.status(200).json({ success: true, result: cards })
+            } catch (error) {
+                res.status(400).json({ success: false, error: error })
+
+            }
+            break;
+        case 'POST':
+            try {
+                const card = await Card.create({ body });
+                res.status(201).json({ success: true, result: card })
+            } catch (error) {
+                res.status(400).json({ success: false, error: error })
+            }
+            break;
+        default:
+            res.status(400).json({ success: false, error: error })
+            break;
+    }
+}
