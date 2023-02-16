14d4ef13a2bbf83bb54edd4d9da5f3133d7e9c58
podmar <marta.podziewska@gmail.com>
2023-02-16 17:22:14 +0100

    remove number

--- models/entryModel.ts
+++ models/entryModel.ts
@@ -2,7 +2,6 @@ import { Schema, model, models, Types } from 'mongoose';
 
 const entrySchema = new Schema({
   date: String,
-  number: Number,
   card: { type: Types.ObjectId , required: true, unique: true, ref: 'Card' },
 }, {timestamps: true });
 
