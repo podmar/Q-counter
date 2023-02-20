c0c32a54fe99fdf9b34199669311c510994a2433
podmar <marta.podziewska@gmail.com>
2023-02-20 15:07:14 +0100

    create a draft of a component to choose a user

---
+++ components/ChooseUser.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+// TODO get all users from users route
+// display all users in a dropdown menu (new component)
+// event choosing a user and linking to entries screen
+// add a user component
+
+
+function ChooseUser() {
+  return (
+        <>
+            <h1>Q-counter</h1>
+            <h3>Choose a user</h3>
+        </>  )
+}
+
+export default ChooseUser
