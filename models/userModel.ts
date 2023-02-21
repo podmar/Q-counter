e7cc0b0fbf6a1bb9c9f171e0658820194219f24f
podmar <marta.podziewska@gmail.com>
2023-02-21 13:36:30 +0100

    move required and unique validation from activeCard to userName

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,8 +1,8 @@
 import { Schema, model, models, Types } from 'mongoose';
 
 const userSchema = new Schema({
-  userName: String,
-  activeCard: { type: Types.ObjectId , required: true, unique: true, ref: 'Card' },
+    userName: { type: String, required: true, unique: true },
+    activeCard: { type: Types.ObjectId, ref: 'Card' },
 }, {timestamps: true });
 
 const User = models.User || model('User', userSchema);
