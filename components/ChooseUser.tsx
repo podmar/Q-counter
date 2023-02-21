9264cd97ab2fa6b4750ddbb47604ce8d6598aadf
podmar <marta.podziewska@gmail.com>
2023-02-21 12:51:42 +0100

    draft fetch function to get all users

--- components/ChooseUser.tsx
+++ components/ChooseUser.tsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-// TODO get all users from users route
-// display all users in a dropdown menu (new component)
-// event choosing a user and linking to entries screen
-// add a user component
+// TODO
+// [x] get all users from users route
+// [ ] display all users in a dropdown menu (new component)
+// [ ] event choosing a user and linking to entries screen
+// [ ] add a user component
 
 
-function ChooseUser( { setUser: ChooseUserProps }) {
+function ChooseUser({ setUser }: ChooseUserProps) {
+    const [users, setUsers] = useState([])
+
+    const requestOptions = {
+        method: "GET",
+    }
+
+    const getUsers = async () => {
+        const response = await fetch("http://localhost:3000/api/users/", requestOptions)
+        const data = await response.json()
+        setUsers(data.results)
+        console.log(data.success, users)
+    }
   return (
         <>
             <h1>Q-counter</h1>
